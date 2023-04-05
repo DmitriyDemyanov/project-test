0cb91ad21ad72b2affe128588c54b51b755b8265
Dmitriy Demyanov <dimaprostokosmos@gmail.com>
2023-04-05 20:30:53 +0300

    first task, home-work - done

--- src/js/app.js
+++ src/js/app.js
@@ -3401,34 +3401,34 @@ countPeople([
 
 // console.log(sortEmotions([':)', 'T_T', ':)', ':D', ':D'], false)); //, [ ':D', ':D', ':)', ':)', 'T_T' ]))
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++SORT+++++++++++++++++++++++++++++++++++++++++++++
-function deepCount(a) {
-	let count = a.length;
-	const idx = a.findIndex((el) => Array.isArray(el));
-	if (idx !== -1) {
-		count = count + deepCount(a[idx]);
-	}
-	return count;
-}
-// console.log(deepCount([1, 2, [3, 4, [5, [1,2,3,4,5]]]]))//, 7, "Expected 7"))
+// function deepCount(a) {
+// 	let count = a.length;
+// 	const idx = a.findIndex((el) => Array.isArray(el));
+// 	if (idx !== -1) {
+// 		count = count + deepCount(a[idx]);
+// 	}
+// 	return count;
+// }
+// // console.log(deepCount([1, 2, [3, 4, [5, [1,2,3,4,5]]]]))//, 7, "Expected 7"))
 
-function sortByValueAndIndex(array){
+// function sortByValueAndIndex(array){
 
-return array.sort((a,b) =>  {
+// return array.sort((a,b) =>  {
 
-	if ((array.indexOf(a)+ 1)* a >  (array.indexOf(b)+ 1)* b ) {
-		console.log('array.indexOf(a)+ 1)* a',(array.indexOf(a)+ 1)* a)
-		console.log('array.indexOf(a)+ 1)* b',(array.indexOf(b)+ 1)* b)
-		console.log('____________________________________________________')
-		return 1
-	}
-	if ((array.indexOf(a)+ 1)* a <  (array.indexOf(b)+ 1)* b ) {
-		return -1
-	}
-	return 0
-})
-}
+// 	if ((array.indexOf(a)+ 1)* a >  (array.indexOf(b)+ 1)* b ) {
+// 		console.log('array.indexOf(a)+ 1)* a',(array.indexOf(a)+ 1)* a)
+// 		console.log('array.indexOf(a)+ 1)* b',(array.indexOf(b)+ 1)* b)
+// 		console.log('____________________________________________________')
+// 		return 1
+// 	}
+// 	if ((array.indexOf(a)+ 1)* a <  (array.indexOf(b)+ 1)* b ) {
+// 		return -1
+// 	}
+// 	return 0
+// })
+// }
 
-console.log(sortByValueAndIndex([ 23, 2, 3, 4, 5 ]))
+// console.log(sortByValueAndIndex([ 23, 2, 3, 4, 5 ]))
 //-------------------------------------------------------Sort an array by value and index ???
 
 // function myLanguages(results) {
@@ -3464,3 +3464,61 @@ console.log(sortByValueAndIndex([ 23, 2, 3, 4, 5 ]))
 
 // console.log(getNiceNames(naughty))
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const map = ["_id", "name", "isActive", "balance"];
+const users = [
+  {
+    "_id": "5d220b10e8265cc978e2586b",
+    "isActive": true,
+    "balance": 2853.33,
+    "age": 20,
+    "name": "Buckner Osborne",
+    "gender": "male",
+    "company": "EMPIRICA",
+    "email": "bucknerosborne@empirica.com",
+    "phone": "+1 (850) 411-2997",
+    "registered": "2018-08-13T04:28:45 -03:00",
+		"nestedField": { total: 300 }
+  },
+  {
+    "_id": "5d220b10144ef972f6c2b332",
+    "isActive": true,
+    "balance": 1464.63,
+    "age": 38,
+    "name": "Rosalie Smith",
+    "gender": "female",
+    "company": "KATAKANA",
+    "email": "rosaliesmith@katakana.com",
+    "phone": "+1 (943) 463-2496",
+    "registered": "2016-12-09T05:15:34 -02:00",
+		"nestedField": { total: 400 }
+  },
+  {
+    "_id": "5d220b1083a0494655cdecf6",
+    "isActive": false,
+    "balance": 2823.39,
+    "age": 40,
+    "name": "Estrada Davenport",
+    "gender": "male",
+    "company": "EBIDCO",
+    "email": "estradadavenport@ebidco.com",
+    "phone": "+1 (890) 461-2088",
+    "registered": "2016-03-04T03:36:38 -02:00",
+		"nestedField": { total: 200 }
+  }
+];
+
+
+	function getUsers(users, map) {
+
+	return	users.reduce((acc, current) => {
+			let obg = {}
+			map.forEach((el) => {
+				obg[el] = current[el]
+			} )
+			acc.push(obg)
+			return acc
+		} ,[]);
+	};
+
+	// console.log(getUsers(users,map))++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	// ask obout reverse?
